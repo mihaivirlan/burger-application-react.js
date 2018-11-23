1feee130e584384c5665762eb27523e8f8da56b0
Mihai <mihaivirlan1@mail.ru>
2018-11-23 21:06:25 +0200

    redirecting the user to the checkout page

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import authReducer from './store/reducers/auth';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
-   burgerBuilder: burgerBuilderReducer,
+    burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
     auth: authReducer
 });
