f8fdb53c4b8de7f00f51662bc7ccd98cc2697aea
Mihai <mihaivirlan1@mail.ru>
2018-11-23 19:50:02 +0200

    forwarding unauthenticated users

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -27,7 +27,7 @@ const buildControls = (props) => (
         <button
             className={classes.OrderButton}
             disabled={!props.purchasable}
-            onClick={props.ordered}>Order Now
+            onClick={props.ordered}>{props.isAuthenticated ? 'Order Now' : 'SIGN UP TO ORDER'}
         </button>
     </div>
 );
